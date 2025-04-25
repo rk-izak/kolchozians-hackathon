32cb187341b29266f4f8b0617278ea916851810e
rk-izak <izakradoslaw@gmail.com>
2025-04-26 00:39:11 +0200

    update agent logic

--- game_agents/king.py
+++ game_agents/king.py
@@ -3,7 +3,6 @@ from agents import Agent, Runner
 import asyncio
 
 class KingState(BaseModel):
-    decision: str
     move_from: str
     move_to: str
     reasoning: str
@@ -12,7 +11,7 @@ class KingPiece:
     def __init__(self, model):
         self.prompt = (
             "You are a King Piece in a game of Chess. "
-            "Your task is to decide on a legal chess move based on current board state and recommendations from other chess pieces. "
+            "Your task is to decide on a legal chess move based on current board state and recommendations from other chess factions. "
             "You must ensure your move is legal for a current board state and chess piece. "
         )
 
@@ -23,13 +22,13 @@ class KingPiece:
             output_type=KingState
         )
 
-    async def call(self, debate: str, board_state: str):
-        result = await Runner.run(self.agent, f"\nDebate State: {debate}\nBoard State: {board_state}")
+    async def call(self, debate: str, board_state: str, legal_moves: list[str]):
+        result = await Runner.run(self.agent, f"\nFaction Statements: {debate}\nBoard State: {board_state}\nAvailable Moves: \n*{'\n*'.join(legal_moves)}")
         return result
 
 async def main():
     king = KingPiece(model='gpt-4.1')
-    result = await king.call("We should move forward", "Test")
+    result = await king.call("We should move forward", "Test", ['e1231', 'f1231'])
     # print(result)
     return result.final_output
 
