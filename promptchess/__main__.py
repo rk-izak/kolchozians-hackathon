8618b989c18ff46fbea2dada0d357c632bc549ac
czyzi0 <czyznikiewicz.mateusz@gmail.com>
2025-04-25 23:19:06 +0200

    Add gradio to dependencies. Move code a bit

--- promptchess/__main__.py
+++ promptchess/__main__.py
@@ -1,9 +1,52 @@
-import chess
+import gradio as gr
+
+from .logic import Board
+
+
+CSS = """
+.cell-button {
+    width: 70px !important;
+    height: 70px !important;
+    font-size: 48px !important;
+    padding: 0 !important;
+}
+.figure-img {
+    font-size: 128px !important;
+}
+"""
+
+
+def make_board(board, thinking_img, thinking):
+    with gr.Row():
+        for col in range(8):
+            with gr.Column(min_width=70, scale=0): 
+                for row in range(8):
+                    piece = board.board[row][col].visualize()
+                    gr.Button(
+                        value=piece,
+                        elem_classes=["cell-button"]
+                    ).click(
+                        fn=lambda r=row, c=col: (board.board[r][c].visualize(), board.board[r][c].description),
+                        outputs=(thinking_img, thinking)
+                    )
 
 
 def main():
-    board = chess.Board()
-    print(board)
+    board = Board()
+    # board = chess.Board()
+    # print(board)
+    with gr.Blocks(css=CSS) as demo:
+        gr.Markdown("### PromptChess")
+        _ = gr.Textbox(label="Clicked Square (row,col)", interactive=False)
+        with gr.Row():
+            with gr.Column(scale=1):
+                with gr.Row():
+                    thinking_img = gr.Textbox(label="Figure", elem_classes=["figure-img"])
+                    thinking = gr.Textbox(label="Thinking...", interactive=False, elem_classes=["figure-img"])
+            with gr.Column(min_width=760, scale=0):
+                make_board(board, thinking_img, thinking)
+
+    demo.launch()
 
 
 if __name__ == "__main__":
--- promptchess/logic.py
+++ promptchess/logic.py
@@ -1,6 +1,8 @@
-
 import re
 import uuid
+
+import chess
+
 
 class State():
     def __init__(self, name):
@@ -38,32 +40,30 @@ class State():
 
 
 class Board():
+
     def __init__(self):
         self.board = []
         for _ in range(8):
             self.board.append([State('.') for _ in range(8)])
-        
+
         for col in range(8):
             self.board[6][col] = State(f'p{col}')
             self.board[1][col] = State(f'P{col}')
-    
+
         self.board[0][0] = State(f'R{0}')
         self.board[0][7] = State(f'R{7}')
         self.board[7][0] = State(f'r{0}')
         self.board[7][7] = State(f'r{7}')
-
 
         self.board[0][1] = State(f'N{1}')
         self.board[0][6] = State(f'N{6}')
         self.board[7][1] = State(f'n{1}')
         self.board[7][6] = State(f'n{6}')
 
-
         self.board[0][2] = State(f'B{2}')
         self.board[0][5] = State(f'B{5}')
         self.board[7][2] = State(f'b{2}')
         self.board[7][5] = State(f'b{5}')
-    
 
         self.board[0][3] = State(f'Q{3}')
         self.board[0][4] = State(f'K{4}')
